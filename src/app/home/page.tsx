ae0abf19e88c7c00e83bbf6bcf983887f10dcc02
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-14 09:52:28 -0300

    feito estrutura page 2

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const HomePage = () => {
   return (
     <section style={{ padding: "2rem" }}>
---
+++ src/app/menu2/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu2Page = () => {
+  return (
+    <section style={{ padding: "2rem" }}>
+      <h2>Página Menu 2</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui blanditiis
+        cum accusantium nisi animi dicta laudantium. Sint molestias aliquam quod
+        sunt autem repellendus fuga quas, maiores esse, omnis consequatur amet.
+      </p>
+    </section>
+  );
+};
+
+export default Menu2Page;
