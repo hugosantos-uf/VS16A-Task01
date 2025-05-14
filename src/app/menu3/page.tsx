a6d049547240d6759a8afa1dac982264b6840912
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-14 10:02:15 -0300

    feito estrutura page #

---
+++ src/app/menu3/page.tsx
@@ -0,0 +1,14 @@
+const Menu3Page = () => {
+  return (
+    <section style={{ padding: "2rem" }}>
+      <h2>Página Menu 3</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad rem magni
+        sapiente vero obcaecati nobis at quae sequi culpa, autem ab provident
+        dolorem! Dolor blanditiis dicta odio, sunt accusantium provident.
+      </p>
+    </section>
+  );
+};
+
+export default Menu3Page;
