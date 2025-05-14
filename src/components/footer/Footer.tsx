3c210bf004a7ef28a4d6544924e4269cfa3ebb38
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-14 20:30:44 -0300

    aterado footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./footer.module.scss";
 
 const Footer = () => {
-  return <footer className={styles.footer}>Todos os diretos reservados</footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>© 2025 Hugo Santos. Todos os direitos reservados.</p>
+      <p>Conecte-se comigo</p>
+    </footer>
+  );
 };
 
 export default Footer;
