2c0450c45e75e49ce5c464aa30e6508d47457a2a
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-14 11:36:11 -0300

    correcoes

--- src/app/menu3/page.tsx
+++ src/app/menu3/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./contato.module.scss";
 
 const Menu3Page = () => {
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./header.module.scss";
 
 const Header = () => {
