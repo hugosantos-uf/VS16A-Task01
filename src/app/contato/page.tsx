6ac91c5cf4c6f0438b54bac2a6e7406d87099a57
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-15 10:20:49 -0300

    feito page contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,18 +1,48 @@
 import React from "react";
 import styles from "./contato.module.scss";
 
-const Menu3Page = () => {
+const Contato = () => {
   return (
     <section className={styles.container}>
-      <h2>Entre em Contato</h2>
-      <form className={styles.form}>
-        <input type="text" placeholder="Seu nome" required />
-        <input type="email" placeholder="Seu email" required />
-        <textarea placeholder="Sua mensagem..." rows={4} required />
-        <button type="submit">Enviar</button>
-      </form>
+      <p className={styles.hello}>Conecte-se comigo</p>
+      <h5>Com um clique</h5>
+      <div className={styles.intro}>
+        <div className={styles.conteudo}>
+          <p className={styles.texto}>
+            Eu adoraria ouvir de você! Se você possui qualquer dúvida,
+            comentários ou feedback, por favor use o formulário abaixo.
+          </p>
+          <form className={styles.formulario}>
+            <label>
+              <input
+                type="text"
+                name="nome"
+                placeholder="Digite seu nome"
+                required
+              />
+            </label>
+            <label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Digite seu email"
+                required
+              />
+            </label>
+            <label>
+              <textarea
+                name="mensagem"
+                placeholder="Digite seu mensagem"
+                rows={5}
+                required
+              />
+            </label>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
     </section>
   );
 };
 
-export default Menu3Page;
+export default Contato;
