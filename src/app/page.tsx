a6153d35e633f7e8a0c1195e34c9cf7701e02fc3
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-15 11:18:39 -0300

    corrigido layout start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import Start from "./home/page";
 export default function Home() {
   return (
     <>
-      <section style={{ maxWidth: "90%", margin: "auto" }}>
-        <Start />
-      </section>
+      <Start />
     </>
   );
 }
