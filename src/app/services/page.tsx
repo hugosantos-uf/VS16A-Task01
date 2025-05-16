1c16d9a64afc3956a3827e8eda5ec6901612caac
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-16 07:32:56 -0300

    feito uso de hook nas paginas de servicos e trabalhos

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./services.module.scss";
-import Card from "@/components/card/Card";
+import ListaDeCards from "@/components/cardList/CardList";
 
 const Services = () => {
   return (
@@ -16,22 +16,7 @@ const Services = () => {
             negócio.
           </p>
           <div className={styles.cards}>
-            <Card
-              titulo="Desenvolvimento Web"
-              texto="Criação de aplicações modernas, responsivas e performáticas utilizando tecnologias como React, Next.js e TypeScript. Ideal para empresas que desejam uma presença digital robusta e escalável."
-            />
-            <Card
-              titulo="Automação de Testes"
-              texto="Implantação de testes automatizados para garantir a qualidade do seu software. Uso de ferramentas como Cypress, Selenium e Postman para testes funcionais, de API e UI."
-            />
-            <Card
-              titulo="Criação de APIs"
-              texto="Desenvolvimento de APIs RESTful seguras, escaláveis e com autenticação JWT. Integração com bancos de dados PostgreSQL e foco em boas práticas de arquitetura backend."
-            />
-            <Card
-              titulo="Devops CI/CD"
-              texto="Integração contínua e entrega contínua com GitHub Actions, Docker, Allure Reports e notificações em canais como Discord. Ideal para automatizar fluxos de teste e deploy em projetos reais."
-            />
+            <ListaDeCards url="/data/servicos.json" />
           </div>
         </div>
       </div>
